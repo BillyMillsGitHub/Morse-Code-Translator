e7094639bc1314934517218f638607c284752432
BillyMillsGitHub <105277453+BillyMillsGitHub@users.noreply.github.com>
2022-06-29 23:01:29 +0100

    Adding Morse Code Translator to Github repository.

---
+++ runner.java
@@ -0,0 +1,16 @@
+
+package MorseCodeTranslator;
+
+public class runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MorseTranslator translator = new MorseTranslator();
+		System.out.println(translator.translate(".... . -.--"));
+		System.out.println(translator.translate(".--- .- ...- .- / .. ... / -.-. --- --- .-.. / --- -.-"));
+
+	}
+	
+
+}
+
